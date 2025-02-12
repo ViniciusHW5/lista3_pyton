91b1583402af7a23f0d86fdedf44f10cfa362aac
ViniciusHW5 <Vinicius_wilbert@estudante.sesisenai.org.br>
2025-02-11 22:14:34 -0300

    não lembro o que eu mudei

--- exe6.py
+++ exe6.py
@@ -1,2 +1,10 @@
 # Pergunte a idade do usuário. Se tiver 16 anos ou mais, exiba a mensagem "Você pode votar", se tiver 18 anos, exiba a mensagem "Você pode aprender a dirigir", 
 # se tiver 14 anos, exiba a mensagem "Você pode comprar um bilhete de loteria", se tiver menos de 14 anos, exiba a mensagem "Você pode fazer doces ou travessuras". 
+num1 =  int(input('digite sua idade '))
+if num1 >=18:
+    print("Você pode aprender a dirigir:")
+else:    
+    if num1 >=16:
+        print("Você pode votar:")
+    else:
+         print("Você pode fazer doces ou travessuras:")  
--- exe7.py
+++ exe7.py
@@ -1,2 +1,10 @@
 # Peça ao usuário para inserir 1, 2 ou 3. Se ele inserir um 1, exiba a mensagem "Obrigado", se ele inserir um 2, exiba "Muito bem", se ele inserir um 3, exiba "Correto". 
 # Se ele inserir qualquer outra coisa, exiba "Mensagem de erro".
+num1 =  int(input('digite 1, 2 ou 3: '))
+if num1 >=2:
+    print("Muito bem")
+#else:    
+    if num1 >=1:
+        print("Obrigado")
+   # else:
+    #     print("Você pode fazer doces ou travessuras")
