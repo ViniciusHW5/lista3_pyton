cebbc34fd90c5ecbf03518481d44e5787b52956a
ViniciusHW5 <Vinicius_wilbert@estudante.sesisenai.org.br>
2025-02-13 19:23:20 -0300

    12 mudou

--- exe12.py
+++ exe12.py
@@ -1,7 +1,7 @@
 # Escreva um programa que leia dois números e que pergunte qual operação você deseja realizar. Você deve poder calcular a soma (+), subtração (-), multiplicação (*) e divisão (/). 
 # Exiba o resultado da operação solicitada. (usar ELIF)
-num1 = float(input("Digite o primeiro número"))
-num2 = float(input("Digite o segundo número"))
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
 print("1-SOMA")
 print("2-SUBTRAÇÃO")
 print("3-MUTIPLICAÇÃO")
@@ -10,4 +10,16 @@ escolha = int(input("Digite o número correpondente a opção desejada: "))
 if escolha == 1:
     soma=num1+num2
     print("Resultado da soma: ",soma)
-    
+    print("Vinicius Haskel Wilbert")    
+if escolha == 2:
+    sub=num1-num2
+    print("Resultado da soma: ",sub)
+    print("Vinicius Haskel Wilbert")    
+if escolha == 3:
+    mut=num1*num2
+    print("Resultado da soma: ",mut)
+    print("Vinicius Haskel Wilbert")    
+if escolha == 4:
+    mut=num1/num2
+    print("Resultado da soma: ",mut)
+    print("Vinicius Haskel Wilbert")
